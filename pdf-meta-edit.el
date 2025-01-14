74af23554ee929c191c734794f322cbb854f880d
Kristoffer Balintona <krisbalintona@gmail.com>
2025-01-14 17:48:21 -0600

    Bump package version

--- pdf-meta-edit.el
+++ pdf-meta-edit.el
@@ -5,7 +5,7 @@
 ;; Author: Kristoffer Balintona <krisbalintona@gmail.com>
 ;; URL: https://github.com/krisbalintona/pdf-meta-edit
 ;; Keywords: files, data
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Package-Requires: ((emacs "24.3") (compat "29.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
